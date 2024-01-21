18fc05c1ccd7089c268c2e50743f37ade5b811c4
laurynas <unextbusinessman@gmail.com>
2024-01-21 10:37:05 +0100

    refactor: commenting out background

--- app/routes/_public+/_layout.tsx
+++ app/routes/_public+/_layout.tsx
@@ -5,11 +5,16 @@ const Layout = () => {
   return (
     <>
       <Navbar />
-      <div className="h-full flex-1 max-w-7xl mx-auto relative container mt-14 md:mt-28 ">
+      <div className="h-full flex-1 relative">
+        {/* <img
+          src="https://png.pngtree.com/png-vector/20210923/ourlarge/pngtree-white-grid-line-pattern-on-a-gray-png-image_3949647.png"
+          alt="background"
+          className="fixed top-0  w-full opacity-5 z-0"
+        /> */}
         <Outlet />
       </div>
-      <div className="max-w-7xl mx-auto container">
-        <div>Footer</div>
+      <div className="border-t border-muted py-6">
+        <div className="max-w-7xl mx-auto container">Footer</div>
       </div>
     </>
   );
