6abbb696c8239dcc524cd484d0576313a50a7b27
laurynas <unextbusinessman@gmail.com>
2024-01-21 10:35:08 +0100

    refactor(style): Adding tailwind styling restructuring code

--- app/components/layout/hero.tsx
+++ app/components/layout/hero.tsx
@@ -4,12 +4,12 @@ import { Button } from '../ui/button';
 
 export const Hero = () => {
   return (
-    <div className="flex flex-col items-center text-center mb-12 md:mb-28 py-28">
+    <div className="flex sticky top-0 right-0 h-screen w-full flex-col items-center justify-center py-28">
       <Link
         to="https://github.com/unext1"
         target="_blank"
         rel="noreferrer"
-        className="inline-flex items-center rounded-lg bg-secondary/40 px-3 py-1 text-sm font-medium"
+        className="inline-flex items-center rounded-lg bg-secondary/30 px-3 py-1 text-sm font-medium"
       >
         <GitHubLogoIcon />{' '}
         <div data-orientation="vertical" role="none" className="shrink-0 bg-border w-[1px] mx-2 h-4"></div>{' '}
@@ -19,18 +19,20 @@ export const Hero = () => {
         Hi there
         <img src="../wave.webp" className="flex h-4 pl-1 my-auto" alt="wave emoji" />, I&apos;m
       </p>
-      <h2 className="text-6xl -mt-4 font-extrabold xl:text-7xl 2xl:text-8xl">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-foreground">Laurynas.</span>
-      </h2>
-      <h2 className="mt-2 text-base text-muted-foreground">
+      <h1 className="text-6xl -mt-4 font-extrabold xl:text-7xl 2xl:text-8xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary via-primary to-foreground">
+          Laurynas.
+        </span>
+      </h1>
+      <p className="mt-2 text-base text-muted-foreground text-center px-8">
         Turning your ideas into real life products is my calling.
-      </h2>
+      </p>
       <div className="flex gap-2 mt-6">
-        <Button size="sm" variant="outline">
-          About
+        <Button asChild size="sm" variant="outline">
+          <Link to="#about">About</Link>
         </Button>
-        <Button size="sm" variant="default">
-          Something
+        <Button asChild size="sm" variant="default">
+          <Link to="#portfolio">Portfolio</Link>
         </Button>
       </div>
     </div>
