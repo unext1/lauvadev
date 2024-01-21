a37ecfbb16c7fdd5d72a33e7b90749e58fe2c716
laurynas <unextbusinessman@gmail.com>
2024-01-21 10:38:04 +0100

    refactor: Adding components to index page

--- app/routes/_public+/index.tsx
+++ app/routes/_public+/index.tsx
@@ -1,19 +1,40 @@
 import type { MetaFunction } from '@remix-run/node';
 import { About } from '~/components/layout/about';
+import ContactComponent from '~/components/layout/contact';
 
 import { Hero } from '~/components/layout/hero';
 import { PortfolioComponent } from '~/components/layout/portfolio';
+import { Skills } from '~/components/layout/skills';
 
 export const meta: MetaFunction = () => {
   return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
 };
 
+const websites = [
+  {
+    title: 'Menulio Ritmu.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sequi magnam reiciendis, autem, ipsum molestiae aspernatur ad quisquam commodd.',
+    image: '/project4.png',
+    badges: ['Remix.run', 'Tailwind', 'API']
+  },
+  {
+    title: 'Home By Aurelija.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sequi magnam reiciendis, autem, ipsum molestiae aspernatur ad quisquam commodd.',
+    image: '/project5.jpg',
+    badges: ['Remix.run', 'Tailwind']
+  }
+];
+
 export default function Index() {
   return (
     <div>
       <Hero />
-      <PortfolioComponent />
+      <Skills />
       <About />
+      <PortfolioComponent websites={websites} ctaButton />
+      <ContactComponent />
     </div>
   );
 }
