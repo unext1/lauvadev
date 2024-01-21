30e82ee21f3c89a475206cc436cda6dc9d63c50e
laurynas <unextbusinessman@gmail.com>
2024-01-21 10:38:21 +0100

    feat: Work route page

---
+++ app/routes/_public+/work.tsx
@@ -0,0 +1,42 @@
+import { PortfolioComponent } from '~/components/layout/portfolio';
+
+const websites = [
+  {
+    title: 'Menulio Ritmu.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sequi magnam reiciendis, autem, ipsum molestiae aspernatur ad quisquam commodd.',
+    image: '/project4.png',
+    badges: ['Remix.run', 'Tailwind', 'API']
+  },
+  {
+    title: 'Home By Aurelija.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sequi magnam reiciendis, autem, ipsum molestiae aspernatur ad quisquam commodd.',
+    image: '/project5.jpg',
+    badges: ['Remix.run', 'Tailwind']
+  },
+  {
+    title: 'Home By Aurelija.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sequi magnam reiciendis, autem, ipsum molestiae aspernatur ad quisquam commodd.',
+    image: '/project5.jpg',
+    badges: ['Remix.run', 'Tailwind']
+  },
+  {
+    title: 'Home By Aurelija.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sequi magnam reiciendis, autem, ipsum molestiae aspernatur ad quisquam commodd.',
+    image: '/project5.jpg',
+    badges: ['Remix.run', 'Tailwind']
+  }
+];
+
+const WorkPage = () => {
+  return (
+    <div className="mt-12 ">
+      <PortfolioComponent websites={websites} />
+    </div>
+  );
+};
+
+export default WorkPage;
