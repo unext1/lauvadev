eac1f81302f65e9ed2c7f9279dad45a102897c7f
laurynas <unextbusinessman@gmail.com>
2024-01-23 17:38:04 +0100

    refactor: Fixed props and styling

--- app/components/layout/portfolio.tsx
+++ app/components/layout/portfolio.tsx
@@ -1,10 +1,12 @@
 import { $path } from 'remix-routes';
-import { Badge } from '../ui/badge';
 import { Link } from '@remix-run/react';
+
+import { Badge } from '../ui/badge';
 
 export const PortfolioComponent = ({
   websites,
-  ctaButton
+  ctaButton,
+  badgeTitle
 }: {
   websites: {
     title: string;
@@ -13,20 +15,21 @@ export const PortfolioComponent = ({
     badges: string[];
   }[];
   ctaButton?: boolean;
+  badgeTitle: string;
 }) => {
   return (
-    <div className="flex flex-col items-center relative section-padding z-10 bg-background " id="portfolio">
-      <div className="my-auto mx-auto container max-w-7xl  ">
+    <div className="flex flex-col items-center relative section-padding z-10 bg-background" id="portfolio">
+      <div className="my-auto mx-auto container max-w-7xl ">
         <div className="text-center">
           <Badge variant="outline" className="px-6 py-2 text-center">
-            Portfolio
+            {badgeTitle}
           </Badge>
-          <h3 className="mt-2 text-3xl sm:text-4xl lg:text-5xl mb-2 font-bold">My Work.</h3>
+          <h3 className="mt-2 text-3xl sm:text-4xl lg:text-5xl mb-2 font-bold">My Work</h3>
         </div>
         <div className="my-8 space-y-10">
           {websites.map((website) => (
             <div
-              className="px-8 py-12 bg-secondary/30 rounded-xl grid gap-8 md:gap-12 grid-cols-1 lg:grid-cols-3 group"
+              className=" px-4 py-6 md:px-8 md:py-12 bg-secondary/30 rounded-xl grid gap-8 md:gap-12 grid-cols-1 lg:grid-cols-3 group"
               key={website.title}
             >
               <div className="col-span-1 order-2 my-auto">
@@ -45,13 +48,13 @@ export const PortfolioComponent = ({
               <img
                 src={website.image}
                 alt="project"
-                className="rounded-xl lg:col-span-2 order-1 group-hover:scale-105 transition"
+                className="rounded-xl lg:col-span-2 order-1 md:group-hover:scale-105 transition"
               />
             </div>
           ))}
         </div>
         {ctaButton && (
-          <div className="text-center">
+          <div className="text-center transition hover:scale-95 w-fit mx-auto underline underline-offset-4">
             <Link to={$path('/work')} className="text-xs pb-1 uppercase font-bold">
               Check out more
             </Link>
