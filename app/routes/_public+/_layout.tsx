a7149adca703072cb3fd2cccc460aca3d9ad0a9e
laurynas <unextbusinessman@gmail.com>
2024-02-15 15:56:28 +0100

    refactor: removed comments

--- app/routes/_public+/_layout.tsx
+++ app/routes/_public+/_layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from '@remix-run/react';
-import Footer from '~/components/layout/footer';
 
+import Footer from '~/components/layout/footer';
 import Navbar from '~/components/layout/navbar';
 
 const Layout = () => {
@@ -8,11 +8,6 @@ const Layout = () => {
     <>
       <Navbar />
       <div className="h-full flex-1 relative">
-        {/* <img
-          src="https://www.lokkeestudios.com/_astro/future-grid.a0cd4189.webp"
-          alt="background"
-          className="h-screen absolute top-0 w-full opacity-5 animate-pulse z-20"
-        /> */}
         <Outlet />
       </div>
       <div className="border-t border-muted py-6">
