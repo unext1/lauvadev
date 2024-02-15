c7da7068f7574d71acd25baa4f8612e11d3a4bbe
laurynas <unextbusinessman@gmail.com>
2024-02-15 15:52:52 +0100

    refactor: Added animations

--- app/components/layout/hero.tsx
+++ app/components/layout/hero.tsx
@@ -1,41 +1,95 @@
-import { GitHubLogoIcon } from '@radix-ui/react-icons';
+import { ArrowRightIcon, GitHubLogoIcon } from '@radix-ui/react-icons';
 import { Link } from '@remix-run/react';
+import { motion } from 'framer-motion';
+import { MouseIcon } from 'lucide-react';
 
 import { Button } from '../ui/button';
+import { OptimizedImage } from '../image';
 
 export const Hero = () => {
   return (
-    <div className="flex mt-12 py-28 md:py-40 flex-col items-center justify-center">
-      <Link
-        to="https://github.com/unext1"
-        target="_blank"
-        rel="noreferrer"
-        className="inline-flex items-center rounded-lg bg-secondary/30 px-3 py-1 text-base font-medium"
+    <div className="flex h-screen lg:h-fit lg:py-40 2xl:mt-24 flex-col items-center justify-center">
+      <motion.div
+        initial={{ y: -10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4 }}
+        className="relative group cursor-pointer"
       >
-        <GitHubLogoIcon />{' '}
-        <div data-orientation="vertical" role="none" className="shrink-0 bg-border w-[1px] mx-2 h-4"></div>{' '}
-        <span className="inline">Follow me on Github</span>
-      </Link>
-      <p className="flex text-base mt-6">
+        <div className="absolute inset-1 blur rounded-md z-0 bg-gradient-to-r from-primary to-accent animate-pulse" />
+        <Link
+          to="https://github.com/unext1"
+          target="_blank"
+          rel="noreferrer"
+          className="inline-flex z-10 relative items-center rounded-md bg-background px-3 py-1 text-sm font-medium"
+        >
+          <GitHubLogoIcon />{' '}
+          <div data-orientation="vertical" role="none" className="shrink-0 bg-border w-[1px] mx-2 h-4"></div>{' '}
+          <span className="inline">Follow me on Github </span>
+          <ArrowRightIcon className="ml-2" />
+        </Link>
+      </motion.div>
+
+      <motion.p
+        initial={{ y: -10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.1 }}
+        className="flex text-base mt-6"
+      >
         Hi there
-        <img src="../wave.webp" className="flex h-4 pl-1 my-auto" alt="wave emoji" />, I&apos;m
-      </p>
-      <h1 className="text-6xl -mt-2 font-extrabold xl:text-7xl 2xl:text-8xl">
+        <OptimizedImage
+          src="../wave.webp"
+          className="flex h-4 pl-1 my-auto animate-wiggle"
+          alt="wave emoji"
+          layout="fullWidth"
+          height={16}
+        />
+        , I&apos;m
+      </motion.p>
+      <motion.h1
+        initial={{ y: -10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.2 }}
+        className="text-6xl -mt-4 font-extrabold xl:text-7xl 2xl:text-8xl"
+      >
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary via-primary to-foreground">
-          Laurynas.
+          Laurynas
         </span>
-      </h1>
-      <p className="mt-4 text-muted-foreground text-center px-8">
+      </motion.h1>
+      <motion.p
+        initial={{ y: -10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.3 }}
+        className="mt-4 text-muted-foreground text-center px-8"
+      >
         Turning your ideas into real life products is my calling.
-      </p>
-      <div className="flex gap-2 mt-6">
-        <Button asChild variant="outline">
-          <Link to="#about">About</Link>
-        </Button>
-        <Button asChild variant="default">
-          <Link to="#portfolio">Portfolio</Link>
-        </Button>
-      </div>
+      </motion.p>
+      <motion.div
+        initial={{ y: -10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.4 }}
+        className="flex gap-2 mt-6"
+      >
+        <div>
+          <Button asChild variant="outline" className="md:h-10 h-8 px-3 md:px-8">
+            <Link to="#about">About</Link>
+          </Button>
+        </div>
+        <div>
+          <Button asChild variant="default" className="md:h-10 h-8 px-3 md:px-8">
+            <Link to="#portfolio">Portfolio</Link>
+          </Button>
+        </div>
+      </motion.div>
+      <motion.div
+        initial={{ y: -10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.4, delay: 0.5 }}
+        className="flex text-xs items-center mt-8"
+      >
+        <a href="#skills" className="flex animate-bounce">
+          <MouseIcon className="w-4 h-4 animate-wiggle" /> Scroll to see more content
+        </a>
+      </motion.div>
     </div>
   );
 };
--- app/components/layout/portfolio.tsx
+++ app/components/layout/portfolio.tsx
@@ -2,6 +2,10 @@ import { $path } from 'remix-routes';
 import { Link } from '@remix-run/react';
 
 import { Badge } from '../ui/badge';
+import { motion } from 'framer-motion';
+import { sectionVariant } from '~/lib/section-variant';
+import { Button } from '../ui/button';
+import { OptimizedImage } from '../image';
 
 export const PortfolioComponent = ({
   websites,
@@ -13,12 +17,20 @@ export const PortfolioComponent = ({
     description: string;
     image: string;
     badges: string[];
+    href: string;
   }[];
   ctaButton?: boolean;
   badgeTitle: string;
 }) => {
   return (
-    <div className="flex flex-col items-center relative section-padding z-10 bg-background" id="portfolio">
+    <motion.div
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 'some' }}
+      variants={sectionVariant}
+      className="flex flex-col items-center relative section-padding z-10 bg-background scroll-m-20"
+      id="portfolio"
+    >
       <div className="my-auto mx-auto container max-w-7xl ">
         <div className="text-center">
           <Badge variant="outline" className="px-6 py-2 text-center">
@@ -29,13 +41,13 @@ export const PortfolioComponent = ({
         <div className="my-8 space-y-10">
           {websites.map((website) => (
             <div
-              className=" px-4 py-6 md:px-8 md:py-12 bg-secondary/30 rounded-xl grid gap-8 md:gap-12 grid-cols-1 lg:grid-cols-3 group"
+              className=" px-4 py-6 md:px-8 md:py-12 bg-secondary/30 rounded-md grid gap-8 md:gap-12 grid-cols-1 lg:grid-cols-3 group"
               key={website.title}
             >
               <div className="col-span-1 order-2 my-auto">
-                <span className="font-bold text-2xl tracking-wide">{website.title}</span>
+                <span className="font-bold text-2xl lg:text-3xl">{website.title}</span>
 
-                <p className="text-sm text-muted-foreground mt-2">{website.description}</p>
+                <p className="text-sm  text-muted-foreground mt-2">{website.description}</p>
                 <p className="mt-4 text-sm text-muted-foreground">Created with love:</p>
                 <div className="flex flex-wrap gap-2 mt-2">
                   {website.badges.map((badge, index) => (
@@ -44,12 +56,15 @@ export const PortfolioComponent = ({
                     </Badge>
                   ))}
                 </div>
+                <Link to={website.href} target="_blank" rel="noreferrer">
+                  <Button className="mt-6" size="sm">
+                    Check it out Live
+                  </Button>
+                </Link>
               </div>
-              <img
-                src={website.image}
-                alt="project"
-                className="rounded-xl lg:col-span-2 order-1 md:group-hover:scale-105 transition"
-              />
+              <div className="rounded-md lg:col-span-2 order-1 md:group-hover:scale-105 transition-all overflow-hidden">
+                <OptimizedImage src={website.image} quality={45} layout="fullWidth" alt="project" />
+              </div>
             </div>
           ))}
         </div>
@@ -61,6 +76,6 @@ export const PortfolioComponent = ({
           </div>
         )}
       </div>
-    </div>
+    </motion.div>
   );
 };
