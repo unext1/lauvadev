078f88f117390f0a3f0364a1727ccc9e5780dec0
laurynas <unextbusinessman@gmail.com>
2024-02-15 15:55:55 +0100

    feat: framet motion animation lib

---
+++ app/lib/section-variant.ts
@@ -0,0 +1,16 @@
+export const sectionVariant = {
+  offscreen: {
+    y: 20,
+    opacity: 0
+  },
+  onscreen: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      bounce: 0.4,
+      duration: 1.4,
+      delay: 0.2
+    }
+  }
+};
