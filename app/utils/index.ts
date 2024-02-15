b2ef800c1cd3aed01442318f37c0225f06e406c4
laurynas <unextbusinessman@gmail.com>
2024-02-15 15:59:19 +0100

    feat: Function to filter out empty params

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -4,3 +4,13 @@ import { twMerge } from 'tailwind-merge';
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const filteredSearchParams = (params: Record<string, string | number | null | undefined>) => {
+  return new URLSearchParams(
+    Object.fromEntries(
+      Object.entries(params)
+        .filter(([_, v]) => v)
+        .map(([k, v]) => [k, v!.toString()])
+    )
+  ).toString();
+};
