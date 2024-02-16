59d9439358288299fe533039f00415ea0bb5e224
laurynas <unextbusinessman@gmail.com>
2024-02-16 13:08:51 +0100

    build: removed unused dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { unstable_vitePlugin as remix } from '@remix-run/dev';
-import { installGlobals } from '@remix-run/node';
 import { remixDevTools } from 'remix-development-tools/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import svgr from 'vite-plugin-svgr';
 
 import remixConfig from './remix.config.mjs';
-
-installGlobals();
 
 export default defineConfig({
   server: {
     port: 3000
   },
-  plugins: [remixDevTools(), remix(remixConfig), tsconfigPaths(), svgr()]
+  plugins: [remixDevTools(), remix(remixConfig), tsconfigPaths()]
 });
