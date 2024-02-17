6fee92e1a7a1f5bbed334e1fd93ba4511cb54879
laurynas <unextbusinessman@gmail.com>
2024-02-17 20:46:27 +0100

    refactor: Removed font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,9 +73,6 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         wiggle: 'wiggle 0.4s ease-in-out infinite'
-      },
-      fontFamily: {
-        inter: ['Inter']
       }
     }
   },
